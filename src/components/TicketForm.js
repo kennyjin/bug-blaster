f61d2743aa390bf12c71a294e19bec328fb8f453
Kenny Jin <jianianjin@gmail.com>
2025-10-02 13:04:21 -0700

    Add title, description, priority and submit button to TicketForm component

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -17,5 +17,50 @@ export default function TicketForm() {
     setPriority("1");
   };
 
-  return <></>;
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Stop the page from reloading when we submit the form
+    clearForm();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="ticket-form">
+      <div>
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          className="form-input"
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label>Description</label>
+        <textarea
+          value={description}
+          className="form-input"
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </div>
+      <fieldset className="priority-fieldset">
+        <legend>priority</legend>
+
+        {Object.entries(priorityLabels).map(([value, label]) => (
+          <label key={value} className="priority-label">
+            <input
+              type="radio"
+              value={value}
+              checked={priority === value}
+              className="priority-input"
+              onChange={(e) => setPriority(e.target.value)}
+            />
+            {label}
+          </label>
+        ))}
+      </fieldset>
+
+      <button type="submit" className="button">
+        Submit
+      </button>
+    </form>
+  );
 }
