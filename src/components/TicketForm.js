a2f338ec9ffa244216032ffafb985249fedec100
Kenny Jin <jianianjin@gmail.com>
2025-10-05 18:12:36 -0700

    Add logic for cancelling an edit procedure

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -47,6 +47,11 @@ export default function TicketForm({ dispatch, editingTicket }) {
     clearForm();
   };
 
+  const handleCancel = () => {
+    dispatch({ type: "CLEAR_EDITING_TICKET" });
+    clearForm();
+  };
+
   return (
     <form onSubmit={handleSubmit} className="ticket-form">
       <div>
@@ -86,6 +91,12 @@ export default function TicketForm({ dispatch, editingTicket }) {
       <button type="submit" className="button">
         Submit
       </button>
+
+      {editingTicket && (
+        <button className="button" onClick={handleCancel}>
+          Cancel Edit
+        </button>
+      )}
     </form>
   );
 }
